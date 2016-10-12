3629b75931768e63d8975be57d5a9fcba4544bc1
luoyingxing <luoyingxing@126.COM>
2016-10-12 18:04:54 +0800

    update

--- app/src/main/java/com/luo/magiclamp/news/ViewPageFragment.java
+++ app/src/main/java/com/luo/magiclamp/news/ViewPageFragment.java
@@ -1,17 +1,29 @@
 package com.luo.magiclamp.news;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.luo.magiclamp.ApiURL;
+import com.luo.magiclamp.Constant;
 import com.luo.magiclamp.R;
+import com.luo.magiclamp.convenient.TestFragment;
+import com.luo.magiclamp.entity.News;
 import com.luo.magiclamp.entity.NewsDetails;
+import com.luo.magiclamp.frame.BaseActivity;
 import com.luo.magiclamp.frame.BaseFragment;
-import com.luo.magiclamp.frame.ui.scroll.ScrollListView;
+import com.luo.magiclamp.frame.network.ApiMsg;
+import com.luo.magiclamp.frame.network.ApiRequest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,12 +33,12 @@ import java.util.List;
  * <p/>
  * create by luoyingxing on 2016/10/09
  */
-public class ViewPageFragment extends BaseFragment {
+public class ViewPageFragment extends Fragment {
     public static final String TABLE_NUM = "tableNum";
     public static final int ITEM_VIEW_TYPE = 2;
     private View mRootView;
 
-    private ScrollListView mListView;
+    private ListView mListView;
     private ListViewAdapter mListViewAdapter;
 
     private int mTableNum;
@@ -45,28 +57,87 @@ public class ViewPageFragment extends BaseFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("ViewPageFragment", "onCreate");
         Bundle arguments = getArguments();
         if (arguments != null) {
             mTableNum = arguments.getInt(TABLE_NUM);
-            mLog.e("mTableNum = " + mTableNum);
+            Log.e("ViewPageFragment", "mTableNum = " + mTableNum);
         }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.e("ViewPageFragment", "onActivityCreated");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_news_details, container, false);
-        init();
+        Log.e("ViewPageFragment", "onCreateView");
         return mRootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.e("ViewPageFragment", "onStart");
+        init();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.e("ViewPageFragment", "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.e("ViewPageFragment", "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.e("ViewPageFragment", "onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.e("ViewPageFragment", "onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.e("ViewPageFragment", "onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.e("ViewPageFragment", "onDetach");
     }
 
     private void init() {
         setAdapter();
+        loadNews();
     }
 
     private void setAdapter() {
-        mListView = (ScrollListView) mRootView.findViewById(R.id.lv_news_details);
+        mListView = (ListView) mRootView.findViewById(R.id.lv_news_details);
         mListViewAdapter = new ListViewAdapter(getContext(), new ArrayList<NewsDetails>());
         mListView.setAdapter(mListViewAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent intent = new Intent(getActivity(), BaseActivity.class);
+//                intent.putExtra(Constant.ARGS_FRAGMENT_NAME, TestFragment.class.getName());
+//                startActivity(intent);
+            }
+        });
     }
 
     private class ListViewAdapter extends ArrayAdapter<NewsDetails> {
@@ -75,20 +146,20 @@ public class ViewPageFragment extends BaseFragment {
             super(context, 0, newsLists);
         }
 
-        @Override
-        public int getCount() {
-            return super.getCount();
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            return ITEM_VIEW_TYPE;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return ITEM_VIEW_TYPE;
-        }
+//        @Override
+//        public int getCount() {
+//            return super.getCount();
+//        }
+//
+//        @Override
+//        public int getItemViewType(int position) {
+//            return ITEM_VIEW_TYPE;
+//        }
+//
+//        @Override
+//        public int getViewTypeCount() {
+//            return ITEM_VIEW_TYPE;
+//        }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
@@ -112,6 +183,29 @@ public class ViewPageFragment extends BaseFragment {
         class ViewHolder {
             TextView title;
         }
+    }
+
+    private void loadNews() {
+        new ApiRequest<News>(ApiURL.API_NEWS_GET_NEWS) {
+            @Override
+            protected void onSuccess(News result) {
+                mListViewAdapter.addAll(result.getData());
+            }
+
+            @Override
+            protected void onFinish(int what) {
+                super.onFinish(what);
+            }
+
+            @Override
+            protected void onError(ApiMsg apiMsg) {
+                super.onError(apiMsg);
+            }
+
+        }.addParam("tableNum", mTableNum)
+                .addParam("pagesize", Constant.PAGE_SIZE_DEFAULT)
+                .get();
+
     }
 
 }
