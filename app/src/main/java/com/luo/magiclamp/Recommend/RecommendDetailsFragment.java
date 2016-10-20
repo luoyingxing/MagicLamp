35914dd8d0c99e1c12419f3ab4443df464857bcc
luoyingxing <luoyingxing@126.COM>
2016-10-20 11:04:30 +0800

    add progressbar and goback

--- app/src/main/java/com/luo/magiclamp/Recommend/RecommendDetailsFragment.java
+++ app/src/main/java/com/luo/magiclamp/Recommend/RecommendDetailsFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.luo.magiclamp.R;
 import com.luo.magiclamp.frame.BaseFragment;
@@ -18,6 +20,7 @@ import com.luo.magiclamp.frame.BaseFragment;
 public class RecommendDetailsFragment extends BaseFragment {
     public static final String PARAM = "url";
     private View mRootView;
+    private ProgressBar mProgressBar;
     private WebView mWebView;
     private String mUrl = null;
 
@@ -54,16 +57,33 @@ public class RecommendDetailsFragment extends BaseFragment {
         initData();
     }
 
+    private int mJumpCount = 0;  //记录跳转的次数，标记页面
+
     private void findView() {
+        mProgressBar = (ProgressBar) mRootView.findViewById(R.id.pb_recommend_details);
         mWebView = (WebView) mRootView.findViewById(R.id.wv_recommend_details);
+
+        mWebView.requestFocusFromTouch();
 
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
+                mJumpCount++;
+                mLog.e("mJumpCount =  " + mJumpCount);
                 return true;
             }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                mProgressBar.setProgress(newProgress);
+            }
+
         });
     }
 
@@ -75,5 +95,16 @@ public class RecommendDetailsFragment extends BaseFragment {
     public void onPause() {
         super.onPause();
         mWebView.reload();
+    }
+
+    @Override
+    public boolean onBackPressed() {
+        if (mJumpCount > 1) {
+            mWebView.goBack();
+            mJumpCount--;
+        } else {
+            mActivity.goBack();
+        }
+        return true;
     }
 }
