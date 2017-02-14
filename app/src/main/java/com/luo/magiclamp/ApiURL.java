86d465f79ae1d6b82feaaad32960344d9d0a3ebc
luoyingxing <luoyingxing@126.com>
2017-02-15 00:58:17 +0800

    change the api from aFanDa

--- app/src/main/java/com/luo/magiclamp/ApiURL.java
+++ app/src/main/java/com/luo/magiclamp/ApiURL.java
@@ -10,8 +10,8 @@ public class ApiURL {
 
     public static final String API_NEWS_GET_NEWS = "http://api.dagoogle.cn/news/get-news"; //新闻
 
-    public static final String API_RECREATION_JOKE_TEXT = "http://apis.baidu.com/showapi_open_bus/showapi_joke/joke_text"; //文本笑话
-    public static final String API_RECREATION_JOKE_IMG = "http://apis.baidu.com/showapi_open_bus/showapi_joke/joke_pic"; //图片笑话
+    public static final String API_RECREATION_JOKE_TEXT = "http://api.avatardata.cn/Joke/NewstJoke"; //文本笑话
+    public static final String API_RECREATION_JOKE_IMG = "http://api.avatardata.cn/Joke/NewstImg"; //图片笑话
 
     public static final String API_CHAT_TURING = "http://apis.baidu.com/turing/turing/turing"; //图灵机器人
     public static final String API_CHAT_CONSTELLATION_CHART = "http://api.avatardata.cn/Constellation/Query"; //星座运势
